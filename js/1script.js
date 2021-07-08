7a676d3bb3b8757a9a9d7f7031f4f494ca38fdc7
Kir <example@gmail.com>
2021-07-08 21:22:11 +0300

    second step

--- js/1script.js
+++ js/1script.js
@@ -58,49 +58,108 @@
 
 // alert(`Ypu name is ${name}`); 
 
-const category = 'toys';
+// const category = 'toys';
 
-console.log('https://someurl.com/' + category + '/' + '5');  // ES5
-console.log(`https://someurl.com/${category}/5`);  //ES6
+// console.log('https://someurl.com/' + category + '/' + '5');  // ES5
+// console.log(`https://someurl.com/${category}/5`);  //ES6
 
-// const user = 'Kir';
+// // const user = 'Kir';
 
-// alert(`Hello, ${user}`);
+// // alert(`Hello, ${user}`);
 
-console.log('arr' + 'object'); // строка
-console.log('4' + 5); // строка
-console.log(4 + +'3'); // число
+// console.log('arr' + 'object'); // строка
+// console.log('4' + 5); // строка
+// console.log(4 + +'3'); // число
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// incr++;
-// decr--;
-// ++incr;
-// --decr;
+// // incr++;
+// // decr--;
+// // ++incr;
+// // --decr;
 
-// console.log(incr++); // 10
-// console.log(decr--); // 10
+// // console.log(incr++); // 10
+// // console.log(decr--); // 10
 
-console.log(++incr); // 11
-console.log(--decr);// 9
+// console.log(++incr); // 11
+// console.log(--decr);// 9
 
-console.log(5%2);
+// console.log(5%2);
 
-console.log(2*4 == 8); //true
-console.log(2*4 == '8'); //true
-console.log(2*4 === '8'); //false
-console.log(2 + 2 * 2 != '8'); //true
+// console.log(2*4 == 8); //true
+// console.log(2*4 == '8'); //true
+// console.log(2*4 === '8'); //false
+// console.log(2 + 2 * 2 != '8'); //true
 
-&& // и
-|| // или
+// && // и
+// || // или
 
-const isChecked = true,
-      isClose = true;
+// const isChecked = true,
+//       isClose = true;
 
-console.log(isChecked && isClose); // true
-console.log(isChecked || isClose); // true
-console.log(!isChecked || !isClose); // false
+// console.log(isChecked && isClose); // true
+// console.log(isChecked || isClose); // true
+// console.log(!isChecked || !isClose); // false
+
+
+
+// if (4 == 9) {
+//     console.log('Ok');
+// } else {
+//     console.log('error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('error');
+// } else if (num > 100) {
+//     console.log('too match');
+// } else {
+//     console.log('ok');
+// }
+
+// (num === 50) ? console.log('ok') : console.log('error'); // тернарные операторы
+
+// const num = 50;
+
+// switch (num) {  // строгое соответствие
+//     case 49:
+//         console.log('wrong');
+//         break;
+//     case 100:
+//         console.log('wrong');
+//         break;
+//     case 50:
+//         console.log('ok');
+//         break;
+//     default:
+//         console.log('not now');
+//         break;
+// }
+
+// let num = 50;
+
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+    
+// }
 
 
 
--- js/script.js
+++ js/script.js
@@ -21,6 +21,20 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 'use strict';
 
 let numberOfFilms = +prompt('How many films have you watched so far?', '');
@@ -33,12 +47,29 @@ const personalMovieDB = {
       private: false
 };
 
-let lastWatchedFilm = prompt('One of the last movies you watched?', '');
-let youRatings = +prompt('How much would you rate it?', '');
-let lastWatchedFilm2 = prompt('One of the last movies you watched?', '');
-let youRatings2 = +prompt('How much would you rate it?', '');
 
-personalMovieDB.movies[lastWatchedFilm] = youRatings;
-personalMovieDB.movies[lastWatchedFilm2] = youRatings2;
+
+for (let i = 0; i < 2; i++) {
+    const lastWatchedFilm = prompt('One of the last movies you watched?', '');
+    const youRatings = +prompt('How much would you rate it?', '');
+
+    if (lastWatchedFilm != null && youRatings != null && lastWatchedFilm != '' && youRatings != '' && lastWatchedFilm.length < 50) {
+        personalMovieDB.movies[lastWatchedFilm] = youRatings;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('watched small amount of films');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('your cool');
+} else if(personalMovieDB.count >= 30) {
+    console.log('your very cool');
+} else {
+    console.log('error');
+}
 
 console.log(personalMovieDB);
